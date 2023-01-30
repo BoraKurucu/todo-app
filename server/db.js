5ee9d9ccd2db95d2861407fa147b73873dcb0a13
Bora Kurucu <borakurucu11@gmail.com>
2023-01-30 18:57:56 +0100

    finished

---
+++ server/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+module.exports = async() => {
+    try{
+        const connectionParams = {
+            useNewUrlParser:true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        }
+        await mongoose.connect(
+            "mongodb://localhost/todo-app",
+            connectionParams
+            )
+            console.log("Conencted to database")
+    }catch(error){
+        console.log("Could not connect to the database",error)
+    }
+}
